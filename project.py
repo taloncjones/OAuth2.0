64d860fab2c6391bb019bb8c560814d602447d48
Talon Jones <taloncjones@gmail.com>
2019-03-02 01:05:15 +0000

    Import additional dependencies

--- project.py
+++ project.py
@@ -5,10 +5,17 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-#New Imports
+# New Imports
 from flask import session as login_session
 import random, string
 
+# New Imports 2
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
