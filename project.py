474abdfd42b1fe63060b42097a49556cde2338c0
Talon Jones <taloncjones@gmail.com>
2019-03-02 00:27:14 +0000

    Update /login to render login.html

--- project.py
+++ project.py
@@ -25,7 +25,8 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.
       digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    # Render the login template
+    return render_template('login.html')
 
 
 #JSON APIs to view Restaurant Information
